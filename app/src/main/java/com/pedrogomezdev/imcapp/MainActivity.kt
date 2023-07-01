72503666bd41f132974d2822e74ffb3bb0bf2810
Pedro Gómez <pedro.gomez.alonso@hotmail.com>
2023-07-01 21:45:44 +0200

    Commit inicial - Subo el proyecto

---
+++ app/src/main/java/com/pedrogomezdev/imcapp/MainActivity.kt
@@ -0,0 +1,171 @@
+package com.pedrogomezdev.imcapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+    private var currentWeight: Int = 70
+    private var currentAge: Int = 18
+    private var currentHeight: Int = 120
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+    private lateinit var btnSubtractWeight: FloatingActionButton
+    private lateinit var btnPlusWeight: FloatingActionButton
+    private lateinit var tvWeight: TextView
+    private lateinit var btnSubtractAge: FloatingActionButton
+    private lateinit var btnPlusAge: FloatingActionButton
+    private lateinit var tvAge: TextView
+    private lateinit var btnCalculate: Button
+
+    // Es como un atributo estático en java. Se puede acceder desde cualquier lado.
+    companion object{
+        const val IMC_KEY = "IMC_RESULT"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    // Inicializa los componentes
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
+        btnSubtractWeight = findViewById(R.id.btnSubtractWeight)
+        btnPlusWeight = findViewById(R.id.btnPlusWeight)
+        tvWeight = findViewById(R.id.tvWeight)
+        btnSubtractAge = findViewById(R.id.btnSubtractAge)
+        btnPlusAge = findViewById(R.id.btnPlusAge)
+        tvAge = findViewById(R.id.tvAge)
+        btnCalculate = findViewById(R.id.btnCalculate)
+
+    }
+
+    // Inicializa los listeners
+    private fun initListeners() {
+
+        // Cuando se pulse en el viewMale o female, cambia el género activo y establece el color
+        // de las tarjetas de género.
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener { _, value, _ ->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            currentHeight = result.toInt()
+            tvHeight.text = "$result cm"
+        }
+
+        btnPlusWeight.setOnClickListener {
+            currentWeight ++
+            setWeight()
+        }
+
+        btnSubtractWeight.setOnClickListener {
+            currentWeight --
+            setWeight()
+        }
+
+        btnPlusAge.setOnClickListener {
+            currentAge ++
+            setAge()
+        }
+
+        btnSubtractAge.setOnClickListener {
+            currentAge --
+            setAge()
+        }
+
+        btnCalculate.setOnClickListener {
+            val result = calculateIMC()
+            navigateToResult(result)
+        }
+    }
+
+    private fun navigateToResult(result: Double) {
+        val intent = Intent(this, ResultIMCActivity::class.java)
+        intent.putExtra(IMC_KEY, result)
+        startActivity(intent)
+    }
+
+    private fun calculateIMC(): Double {
+        // peso / altura^2
+        val df = DecimalFormat("#.##")
+
+        // Tenemos que pasar la altura a metros
+        val imc = currentWeight / (currentHeight.toDouble()/100 * currentHeight.toDouble()/100)
+        val result = df.format(imc).toDouble()
+
+        return result
+    }
+
+    private fun setAge(){
+        tvAge.text = currentAge.toString()
+    }
+
+    private fun setWeight(){
+        tvWeight.text = currentWeight.toString()
+    }
+
+    // Invierte los valores de los géneros
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    // Establece el valor de
+    private fun setGenderColor() {
+        // Establece el color de fondo de las tarjetas y le pasamos por parámetro
+        // el valor devuelto de la función getBackgroundColor)
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+        // Si lo que le pasamos está a true, ponemos el color de seleccionado
+        // si no le ponemos el estandar.
+        val colorReference = if (isSelectedComponent) {
+            R.color.background_component_selected
+        } else {
+            R.color.background_component
+        }
+
+        // El color que almacenamos en colorReference no es el color en sí mismo
+        // sino que es una referencia, por lo que para devolver el color, debemos usar
+        // ContextCompat.getColor
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+        setWeight()
+        setAge()
+    }
+}
---
+++ app/src/main/java/com/pedrogomezdev/imcapp/ResultIMCActivity.kt
@@ -0,0 +1,79 @@
+package com.pedrogomezdev.imcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.pedrogomezdev.imcapp.MainActivity.Companion.IMC_KEY
+
+class ResultIMCActivity : AppCompatActivity() {
+
+    private lateinit var tvResult: TextView
+    private lateinit var tvIMC: TextView
+    private lateinit var tvDescription: TextView
+    private lateinit var btnRecalculate: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_imcactivity)
+
+        val result = intent.extras?.getDouble(IMC_KEY) ?: -1.0
+        initComponents()
+        initUI(result)
+    }
+
+
+    private fun initComponents(){
+        tvResult = findViewById(R.id.tvResult)
+        tvIMC = findViewById(R.id.tvIMC)
+        tvDescription = findViewById(R.id.tvDescription)
+        btnRecalculate = findViewById(R.id.btnRecalculate)
+    }
+
+    private fun initListeners(){
+        // Con onBackPressed volvemos para atrás
+        btnRecalculate.setOnClickListener{ onBackPressed() }
+    }
+
+    private fun initUI(result: Double){
+        tvIMC.text = result.toString()
+
+        when(result){
+            // Bajo peso
+            in 0.00..18.50 -> {
+                tvResult.setTextColor(ContextCompat.getColor(this,R.color.low_weight))
+                tvResult.text = getString(R.string.title_low_weight)
+                tvDescription.text = getString(R.string.description_low_weigh)
+            }
+
+            // Peso normal
+            in 18.51..24.99 -> {
+                tvResult.setTextColor(ContextCompat.getColor(this,R.color.normal_weight))
+                tvResult.text = getString(R.string.title_normal_weight)
+                tvDescription.text = getString(R.string.description_normal_weight)
+            }
+
+            // Sobrepeso
+            in 25.00..29.99 -> {
+                tvResult.setTextColor(ContextCompat.getColor(this,R.color.overweight))
+                tvResult.text = getString(R.string.title_overweight)
+                tvDescription.text = getString(R.string.description_overweight)
+            }
+
+            // Obesidad
+            in 30.00..99.99 -> {
+                tvResult.setTextColor(ContextCompat.getColor(this,R.color.obesity))
+                tvResult.text = getString(R.string.title_obesity)
+                tvDescription.text = getString(R.string.description_obesity)
+            }
+
+            // Si no es ninguno de los anteriores, error
+            else ->     {
+                tvIMC.text = getString(R.string.error)
+                tvResult.text = getString(R.string.error)
+                tvDescription.text = getString(R.string.error)
+            }
+        }
+    }
+}
